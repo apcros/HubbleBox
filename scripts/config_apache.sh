17744aead163b7577b7c06e5f6f787cb361681ab
apcros <apcros.mail@gmail.com>
2016-11-10 00:01:09 +0000

    Oops forgot to enable url rewrite

--- scripts/config_apache.sh
+++ scripts/config_apache.sh
@@ -2,5 +2,5 @@
 echo "=== Configuring Apache ==="
 a2enmod rewrite
 cp /vagrant/config/apache_000-default.conf /etc/apache2/sites-enabled/000-default.conf
+a2enmod rewrite
 service apache2 restart
-/etc/init.d/postgresql restart
